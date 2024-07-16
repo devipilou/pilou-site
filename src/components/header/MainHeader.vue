8111b9e9963629943fa6e1023de3b64c478aba6f
devipilou <devipilou@gmail.com>
2024-07-16 13:49:28 +0200

    fix link styles

--- src/components/header/MainHeader.vue
+++ src/components/header/MainHeader.vue
@@ -4,74 +4,75 @@ import HeaderContent from './HeaderContent.vue'
 </script>
 
 <template>
-    <header>
-        <RouterLink to="/" class="logo-wrapper">
-            <img alt="Pilou Dev logo" class="logo" src="@/assets/LogoPilouDev.png" width="200" height="200" />
-        </RouterLink>
-        <div class="wrapper">
-            <HeaderContent />
+  <header>
+    <RouterLink to="/" class="logo-wrapper">
+      <img alt="Pilou Dev logo" class="logo" src="@/assets/LogoPilouDev.png" width="200" height="200" />
+    </RouterLink>
+    <div class="wrapper">
+      <HeaderContent />
 
-            <nav>
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
-                <RouterLink to="/projects">Projects</RouterLink>
-                <RouterLink to="/resume">Résumé</RouterLink>
-                <RouterLink to="/contact">Contact</RouterLink>
-            </nav>
-        </div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/projects">Projects</RouterLink>
+        <RouterLink to="/resume">Résumé</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
+      </nav>
+    </div>
 
-    </header>
+  </header>
 </template>
 
 <style scoped>
 header {
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    line-height: 1.5;
-    max-height: 100vh;
-    min-width: 30rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  line-height: 1.5;
+  max-height: 100vh;
+  min-width: 30rem;
 }
 
 .logo-wrapper {
-    border-radius: 0.5rem;
+  border-radius: 0.5rem;
 }
 
 .logo {
-    display: block;
-    margin: 0 auto 1rem;
-    cursor: pointer;
+  display: block;
+  margin: 0 auto 1rem;
+  cursor: pointer;
 }
 
 nav {
-    width: 100%;
-    font-size: 12px;
-    text-align: center;
-    margin-top: 2rem;
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
 }
 
 nav a.router-link-exact-active {
-    color: var(--color-text);
+  color: var(--color-text);
 }
 
 nav a.router-link-exact-active:hover {
-    background-color: transparent;
+  background-color: transparent;
 }
 
 nav a {
-    display: inline-block;
-    padding: 0 0.75rem;
-    border-left: 1px solid var(--color-border);
+  display: inline-block;
+  padding: 0 0.75rem;
+  border-left: 1px solid var(--color-border);
+  text-decoration: none;
 }
 
 nav a:first-of-type {
-    border: 0;
+  border: 0;
 }
 
 @media (min-width: 1024px) {
-    nav {
-        font-size: 1rem;
-        padding: 1rem 0;
-    }
+  nav {
+    font-size: 1rem;
+    padding: 1rem 0;
+  }
 }
 </style>
